7ad5ed32d985d2008e635e06974312d9f57ccae5
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-09-26 20:56:41 +0300

    Add laba6

---
+++ laba6/laba6/Program.cs
@@ -0,0 +1,23 @@
+﻿(int, int)[] pairs =
+[
+    (2,4), (0,10), (10,12), (16,20), (25,45), (100,175), (375,400)
+];
+
+foreach (var (a, b) in pairs)
+{
+    Console.WriteLine($"НОД({a},{b}) = {Gcd(a, b)}");
+}
+
+int Gcd(int a, int b)
+{
+    while (b != 0)
+    {
+        var t = b;
+        
+        b = a % b;
+        a = t;
+    }
+    
+    return Math.Abs(a);
+}
+    
