6ac0651d7b944feb64a6b7ec52d3334a88b5bdb6
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-10-05 17:48:40 +0300

    Add laba8.1

---
+++ laba8.1/laba8.1/Program.cs
@@ -0,0 +1,94 @@
+﻿Console.Write("Введите число a: ");
+var a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число b: ");
+var b = int.Parse(Console.ReadLine()!);
+
+var factorsA = PrimeFactorization(a);
+var factorsB = PrimeFactorization(b);
+
+Console.WriteLine($"\nРазложение {a}: {FormatFactorization(factorsA)}");
+Console.WriteLine($"Разложение {b}: {FormatFactorization(factorsB)}");
+
+var gcd = CalculateGcdByPrimeFactors(factorsA, factorsB);
+var lcm = CalculateLcmByPrimeFactors(factorsA, factorsB);
+
+Console.WriteLine($"\nНОД({a},{b}) = {gcd} ({FormatFactorization(PrimeFactorization(gcd))})");
+Console.WriteLine($"НОК({a},{b}) = {lcm} ({FormatFactorization(PrimeFactorization(lcm))})");
+
+Console.WriteLine($"\nПроверка тождества: НОД(a,b)*НОК(a,b) = a * b:" +
+                  $"\n\t{gcd * lcm} == {a * b} - {(gcd * lcm == a * b
+                      ? "Выполняется"
+                      : "Не выполняется")}");
+
+int CalculateGcdByPrimeFactors(Dictionary<int,int> factorsX, Dictionary<int,int> factorsY)
+{
+    var localGcd = 1;
+    
+    foreach (var p in factorsX
+                 .Keys
+                 .Concat(factorsY.Keys)
+                 .Distinct())
+    {
+        factorsX.TryGetValue(p, out var expX);
+        factorsY.TryGetValue(p, out var expY);
+        
+        var minExp = Math.Min(expX, expY);
+        
+        localGcd *= (int)Math.Pow(p, minExp);
+    }
+    
+    return localGcd;
+}
+
+int CalculateLcmByPrimeFactors(Dictionary<int,int> factorsX, Dictionary<int,int> factorsY)
+{
+    var localLcm = 1;
+    
+    foreach (var p in factorsX
+                 .Keys
+                 .Concat(factorsY.Keys)
+                 .Distinct())
+    {
+        factorsX.TryGetValue(p, out var expX);
+        factorsY.TryGetValue(p, out var expY);
+        
+        var maxExp = Math.Max(expX, expY);
+        
+        localLcm *= (int)Math.Pow(p, maxExp);
+    }
+    
+    return localLcm;
+}
+
+Dictionary<int, int> PrimeFactorization(int n)
+{
+    var factors = new Dictionary<int, int>();
+    var num = n;
+    
+    for (var i = 2; i * i <= num; i++)
+    {
+        while (num % i == 0)
+        {
+            factors.TryAdd(i, 0);
+            
+            factors[i]++;
+            
+            num /= i;
+        }
+    }
+    
+    if (num > 1)
+    {
+        factors.TryAdd(num, 0);
+        
+        factors[num]++;
+    }
+    
+    return factors;
+}
+
+string FormatFactorization(Dictionary<int,int> factors)
+{
+    return string.Join(" * ", factors.Select(f => $"{f.Key}^{f.Value}"));
+}
