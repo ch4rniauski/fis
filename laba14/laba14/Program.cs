0df2ce4321e3e8843cf1b77ad0f0609bd7b0f4d3
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-11-02 12:32:02 +0300

    Add laba14

---
+++ laba14/laba14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+
+for (var p = 100; p <= 200; p++)
+{
+    if (IsPrime(p))
+    {
+        Console.WriteLine($"p={p}, Wilson: {CheckWilson(p)}");
+    }
+}
+
+static bool CheckWilson(int p)
+{
+    BigInteger fact = 1;
+    
+    for (var i = 2; i < p; i++)
+    {
+        fact = (fact * i) % p;
+    }
+    
+    return fact == p - 1;
+}
+
+static bool IsPrime(int n)
+{
+    if (n < 2)
+    {
+        return false;
+    }
+    
+    if (n % 2 == 0)
+    {
+        return n == 2;
+    }
+    
+    for (var i = 3; i * i <= n; i += 2)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
